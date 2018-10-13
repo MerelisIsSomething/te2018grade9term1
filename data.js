2e5d37088d26a4c5db598f5cf30374a250806554
Merelis Peralta <merelis.peralta@csedge.org>
2018-10-13 11:49:07 -0400

    Finished data.js

--- data.js
+++ data.js
@@ -3,3 +3,16 @@ let message = "Hello World!";
 console.log(message);
 
 // Type your code below!
+let m = 170;
+let p = 700;
+let s = 400;
+let mood = "I Dont Know Anymore.";
+let greeting = "Hello.";
+let avgLifeMotto = "Wait What?";
+let isARapper = true;
+let isGoodAtFortnite = false;
+let Savage;
+constant READLINE = required("readline sync");
+let userInput = READLINE.question("Type anything and I'll repeat it back!");
+console.log(You typed:${userInput});
+
