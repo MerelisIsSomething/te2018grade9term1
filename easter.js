b53b12b110e5f4801f61b39b78354ac306bcb0a8
Merelis Peralta <merelis.peralta@csedge.org>
2018-10-20 12:56:16 -0400

    objects.js

--- easter.js
+++ easter.js
@@ -3,3 +3,39 @@ const READLINE = require("readline-sync");
 
 // Write your program below!
 // First line should get user input (year) and store it in variable y.
+let y = READLINE.question("What's the year for easter?")
+
+let a = y % 19;
+let b = Math.floor(y / 100);
+let c = y % 100;
+let d = Math.floor(b / 4);
+let e = Math.floor(b % 4);
+let f = Math.floor((b+ 8)/3);
+let g = Math.floor((b - f + 1) / 3);
+let h = Math.floor((19 * a + b - d - g + 15) % 30);
+let i = Math.floor(c / 4);
+let k =c % 4;
+let r = Math.floor((32 + 2 * e + 2 * i - h - k) % 7);
+let m = Math.floor((a + 11 * h + 22 * r) / 451);
+let n = Math.floor((h + r - 7 * m + 114) / 31); 
+let p = Math.floor((h + r - 7 * m + 114) % 31); 
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(k);
+console.log(r);
+console.log(m);
+console.log(n);
+console.log(p);
+
+console.log(`On year ${y}, Easter fell on ${n}/${p + 1}`);
+
+
---
+++ objects.js
@@ -0,0 +1,22 @@
+let spy = {
+	firstName: "James",
+	isSpy: true,
+	lastName: "Bond",
+};
+
+let villian = {
+	villianName: "Killer Claw",
+	isVillain: true,
+	catchphrase: "Money makes MY world go round!",
+	greatestCrime: "Illegally hacked into the most secured banks in the Czech Republic",
+};
+
+let hacker = {
+	newName: "Panda707",
+	nickname: "707",
+	catchphrase: "GRRRRAAAAHHH! I got broads in ATLANTA!",
+	archenemy: "Killer Claw",
+};
+
+console.log(`"I'm ${villian.villianName}, and ${villian.catchphrase}! ${hacker.nickname}, prepare to fall to your doom!"`);
+console.log(`"I, ${hacker.newName}, will make sure ${hacker.archenemy} will persish! Why? Because ${hacker.catchphrase}!"`);
